ef135b51c0b58b78ea5ef1f15a71a07b60d8335e
deepak-k <dkdeepakyo@gmail.com>
2015-12-15 10:21:31 +0530

    commit js file for calendar

--- sites/all/themes/libraryzurb/js/scripts.js
+++ sites/all/themes/libraryzurb/js/scripts.js
@@ -29,7 +29,7 @@
 jQuery( document ).ready(function() {
 
   //console.log("running");
-  jQuery('.view-progress .view-content .views-field-field-sticker-calendar-image .field-content').each(function () {
+  jQuery('.view-calendar-sticker .view-content .views-field-field-sticker-calendar-image .field-content').each(function () {
 
     // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
     // it doesn't need to have a start or end
