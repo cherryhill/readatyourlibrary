882a65247f56913598f41f034d33e2f9d9c6ae4f
highermath <info@chillco.com>
2015-12-29 22:50:24 -0800

    Move application files under docroot

--- sites/all/modules/custom/README.txt
+++
@@ -1,5 +0,0 @@
-Use this directory for custom modules.
-
-Use /sites/all/modules/features for features we create.
-Use /sites/all/modules/patched for any contrib modules that we patch.
-Use /sites/all/modules/contrib for anything downloaded directly from drupal.org.
--- sites/all/modules/features/play_library_program/play-library-program-activities-block.tpl.php
+++
@@ -1,9 +0,0 @@
-<ul class="play-activities-list">
-  <?php foreach($pending as $pending_activity): ?>
-  <li class="pending-activity"><?php print $pending_activity; ?></li>
-  <?php endforeach; ?>
-
-  <?php foreach($completed as $completed_activity): ?>
-  <li class="completed-activity"><?php print $completed_activity; ?></li>
-  <?php endforeach; ?>
-</ul>
