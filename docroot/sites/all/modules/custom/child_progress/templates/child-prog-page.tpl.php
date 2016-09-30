ed9ee88c4c7ae46f1226d483cc2213901dda4b62
deepak-k <dkdeepakyo@gmail.com>
2016-09-30 14:27:30 +0530

    commit children progress module

---
+++ docroot/sites/all/modules/custom/child_progress/templates/child-prog-page.tpl.php
@@ -0,0 +1,34 @@
+<!-- Tpl file for children progress page -->
+<div class="progress-page-wrap">
+	<div class="top-block-wrap">
+
+	   <!-- This is the drupal views block of sticker -->
+
+		<div class="sticker-block"> 
+          <?php print views_embed_view('child_progress', 'block'); ?>
+		</div>
+
+		<!-- This is the reward list won by patron  -->
+
+		<div class="pro-reward-block"> 
+          <?php print views_embed_view('prize_won_for_progress_page', 'block'); ?>
+		</div>
+         
+         <!-- This is the user progress for read activity. -->
+
+		<div class="pro-progress-block"> 
+          <?php 
+            $block = module_invoke('child_progress', 'user_prize_block');
+            print render($block['content']); 
+          ?>
+		</div>
+	</div>
+
+	<!-- This is for calendar div -->
+
+	<div class="calendar-wrapper">
+	  <div id="print_button">Print Calendar</div>
+	  <div id="calendar">&nbsp;</div>
+	</div>
+
+</div>
