3d4fe093a7dd4b21a8b1679601c2e30ea62abbc6
mukeshMukesh12 <mukesh_22n@yahoo.co.in>
2016-12-21 18:37:05 +0530

    adding css and js for play library custom module for making accordian

--- docroot/sites/all/modules/custom/play_library_custom/js/play_library_custom.js
+++ docroot/sites/all/modules/custom/play_library_custom/js/play_library_custom.js
@@ -1,8 +1,8 @@
-/**
+/*
  * function callback for progress page report
  */
 (function($) {
-  
+
   Drupal.behaviors.play_progress_teen = {
     attach: function (context, settings) {
       // select list option with thier link
@@ -14,7 +14,18 @@
           return false;
         }
       });
+      //making accordian
+       $('.accordionWrapper .accordionItem').addClass('close');
+       $('.accordionWrapper .accordionItem legend').click(function(){
 
+         if($(this).parent().parent().hasClass('open')) {
+           $(this).parent().parent().removeClass('open').addClass('close');
+         }else {
+          $('.accordionWrapper .accordionItem').removeClass('open').addClass('close');
+          $(this).parent().parent().removeClass('close').addClass('open');
+
+         }
+       });
       //Making single selection for avatar image
       $(".av_radio.form-radio").change(function () {
         $('.av_radio.form-radio').not(this).prop('checked', false);
