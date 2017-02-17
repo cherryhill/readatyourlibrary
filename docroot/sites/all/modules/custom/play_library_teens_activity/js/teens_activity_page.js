e2823fa40390df67c0e87dc7f1ce26c393168339
deepak-k <dkdeepakyo@gmail.com>
2017-02-17 10:59:55 +0530

    commit custom module and js for create reviews block on all reviews page

--- docroot/sites/all/modules/custom/play_library_teens_activity/js/teens_activity_page.js
+++ docroot/sites/all/modules/custom/play_library_teens_activity/js/teens_activity_page.js
@@ -4,8 +4,14 @@
 (function($) {
 Drupal.behaviors.bfc_api_custom = {
   attach: function (context, settings) {
+  	// Js for activity page.
     var basePath = Drupal.settings.basePath; 
     jQuery( '.view-activities-page-for-teens-site .form-type-select .form-select' ).change(function() {
+      var url = jQuery(this).val();
+      window.location.href = basePath + url;
+    });
+    // Js for create reviews block.
+    jQuery( '.form-item-read-activity .form-select' ).change(function() {
       var url = jQuery(this).val();
       window.location.href = basePath + url;
     });
