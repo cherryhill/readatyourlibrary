be273153308a3a20651cbd294e31d9cb217b88d4
poornima313 <poornima.n@gai.co.in>
2017-05-03 13:04:10 +0530

    bug fixes for ticket #1233

--- docroot/sites/all/themes/adult_programme/templates/node--review_activity.tpl.php
+++ docroot/sites/all/themes/adult_programme/templates/node--review_activity.tpl.php
@@ -125,7 +125,7 @@
       $img_uri = $result->fetchObject();
       $img_uri = $img_uri->uri;
       $style = 'avatar_style';
-      $img_path = image_style_url($img_uri);
+      $img_path = file_create_url($img_uri);
       }
       if(!empty($img_uri)){
         $img = "<img src='$img_path'>";
