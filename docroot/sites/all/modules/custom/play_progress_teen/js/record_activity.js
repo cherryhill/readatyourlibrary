6e993fa7bad6f0ac2df0144cff6fd58ca3e8e7f7
poornima313 <poornima.n@gai.co.in>
2017-05-04 14:20:56 +0530

    adding record activity for teens program

---
+++ docroot/sites/all/modules/custom/play_progress_teen/js/record_activity.js
@@ -0,0 +1,44 @@
+(function($) {
+  Drupal.behaviors.play_progress_teen = {
+    attach: function (context, settings) {
+      $("#patron-activity-record-form #edit-username").change(function() {
+        $('#views-exposed-form-raffle-winners-page #edit-uid').val( this.value );
+      });
+
+      //Update reward pickup
+      $('.update-rew').on('click', function (){
+      	console.log("daas");
+      	var rew_id = this.id;
+      	var pickup_date = $('#staff-date-'+ rew_id + '-datepicker-popup-0').val();
+      	var staff_notes = $('#staff-notes-'+ rew_id).val();
+      	var pick_up_status = $('#staff-status-' + rew_id).val();
+      	
+      	if(pickup_date.length == 0 || staff_notes.length == 0 || $('#staff-status-' + rew_id).checked ){
+	      	alert("Please enter all fields");
+      	}
+      	else{
+      		var baseUrl = Drupal.settings.basePath + 'update-reward-pickup';
+	      	jQuery.ajax({
+	            url: baseUrl,
+	            async: true,
+	            type: 'post',
+	            data: 'reward-id=' + rew_id + '&pickup-date=' + pickup_date + '&staff-notes=' +staff_notes+ '&pickup-status=' +pick_up_status,
+	            //on Successs render drupal set messages
+	            success: function(res){
+                  window.location.reload();
+	              console.log('dsvssdvs');
+	              if(res === 'success'){
+                  console.log('sssss');
+	              	alert("Reward Pickup date updated");
+	              	window.location.reload();
+	              } console.log('wer');
+	            },
+	            error: function(jqXHR, data, error){
+	              alert("Apologies. There is some error in the system. Please refresh page. If problem persists report to system administrator" + error );
+	            }
+	          });
+      	}
+    });
+  }
+}
+}) (jQuery);
